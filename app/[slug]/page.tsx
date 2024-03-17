2d807867ff9e8acb848085ba3a37c73569536f55
Harjot Singh <HarjjotSinghh@gmail.com>
2024-03-17 07:24:25 +0530

    add: redirect to urls added

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,4 +1,14 @@
 "use server"
+
+import { getXataClient } from "@/lib/xata";
+import { redirect } from "next/navigation";
+
 export default async function Page({ params }: { params: { slug: string } }) {
-  return <>{params.slug}</>;
+  const xataClient = getXataClient();
+  const response = await xataClient.db.redirects.select(["*"]).filter({ "slug": params.slug.toString() }).getFirst();
+  // console.log(response);
+  if (!response) {
+    return redirect("/");
+  }
+  return redirect(response.redirect?.toString() ?? "/");
 }
