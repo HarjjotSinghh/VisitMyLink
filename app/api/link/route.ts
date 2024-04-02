d9c7ed813f69dc8870d722c302960ff0382a39ef
Harjot Singh <HarjjotSinghh@gmail.com>
2024-04-03 01:59:45 +0530

    Update route.ts

--- app/api/link/route.ts
+++ app/api/link/route.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getXataClient } from '@/lib/xata';
+
 export async function GET(req: NextRequest, res: NextResponse) {
   const xataClient = getXataClient();
   const response = await xataClient.db["redirects"].select(["*"]).getMany();
   return new NextResponse(JSON.stringify(response));
 }
+
+export const runtime = "edge";
